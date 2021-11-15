6a200d3b8a6db3462f09dc3a48f9b19ef233bdd0
Yonlero <abraaobrg150@gmail.com>
2021-11-15 18:14:22 -0300

    Project HR-Config-Server Created

---
+++ HR-Config-Server/src/test/java/com/abraaofaher/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraaofaher.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
