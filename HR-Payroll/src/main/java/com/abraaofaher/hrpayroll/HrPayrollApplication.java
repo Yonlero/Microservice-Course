ca5638eb7efd325de962869289f16fa8dcd8e875
Yonlero <abraaobrg150@gmail.com>
2021-11-15 17:39:47 -0300

    Hystrix fault tolerance

--- HR-Payroll/src/main/java/com/abraaofaher/hrpayroll/HrPayrollApplication.java
+++ HR-Payroll/src/main/java/com/abraaofaher/hrpayroll/HrPayrollApplication.java
@@ -2,11 +2,13 @@ package com.abraaofaher.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.Collections;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- HR-Payroll/src/main/java/com/abraaofaher/hrpayroll/controller/PaymentController.java
+++ HR-Payroll/src/main/java/com/abraaofaher/hrpayroll/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.abraaofaher.hrpayroll.controller;
 
 import com.abraaofaher.hrpayroll.model.entities.Payment;
 import com.abraaofaher.hrpayroll.model.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,14 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternativa")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days) {
         return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternativa(long workerId, Integer days){
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
     }
 }
