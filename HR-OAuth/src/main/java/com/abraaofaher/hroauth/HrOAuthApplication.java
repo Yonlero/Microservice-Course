73009bd63f9b532d2b24f5ca2fd761176bed9fa9
Yonlero <abraaobrg150@gmail.com>
2021-11-16 19:15:52 -0300

    Project HR-OAuth Created

---
+++ HR-OAuth/src/main/java/com/abraaofaher/hroauth/HrOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.abraaofaher.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrOAuthApplication.class, args);
+    }
+
+}
