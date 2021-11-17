c83013b83331bc07971b6d11649386d8610a5e1e
Yonlero <abraaobrg150@gmail.com>
2021-11-16 22:55:15 -0300

    Cors Configuration

--- HR-ApiGateway-Zuul/src/main/java/com/abraaofaher/hrapigatewayzuul/config/ResourceServerConfig.java
+++ HR-ApiGateway-Zuul/src/main/java/com/abraaofaher/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,12 +1,21 @@
 package com.abraaofaher.hrapigatewayzuul.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -14,9 +23,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     private JwtTokenStore tokenStore;
 
     //Definindo as rotas para cada tipo de perfil
-    private static final String[] PUBLIC = { "/HR-OAuth/oauth/token" };
-    private static final String[] OPERATOR = { "/HR-Worker/**" };
-    private static final String[] ADMIN = { "/HR-Payroll/**", "/HR-User/**" };
+    private static final String[] PUBLIC = {"/HR-OAuth/oauth/token"};
+    private static final String[] OPERATOR = {"/HR-Worker/**"};
+    private static final String[] ADMIN = {"/HR-Payroll/**", "/HR-User/**"};
 
     public ResourceServerConfig(JwtTokenStore tokenStore) {
         this.tokenStore = tokenStore;
@@ -27,6 +36,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.tokenStore(tokenStore);
     }
+
     //Esse cara aqui configura as autorizações
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -35,5 +45,27 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
                 .antMatchers(ADMIN).hasRole("ADMIN")
                 .anyRequest().authenticated();
+        http.cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean =
+                new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
